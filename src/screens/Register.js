c56f9620ef77f9e444358a66eee37acd0eaac8b3
Felipe Vieira da Rocha <felipevieiradarocha@hotmail.com>
2022-12-07 19:46:41 -0300

    remocao estilo avatar

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -35,13 +35,6 @@ const styles = StyleSheet.create({
         marginBottom: 5,
         marginLeft: 4,
         marginRight: 4,
-    },
-    avatar: {
-        alignSelf: 'center',
-        marginTop: 10,
-        marginBottom: 10,
-        backgroundColor: '#a7b0af',
     }
-
 })
 
